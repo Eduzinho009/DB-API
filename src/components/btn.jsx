1aee83d41796a1a57ccc0d5879551fcd45e8d1d6
Eduzinho009 <115952759+Eduzinho009@users.noreply.github.com>
2025-04-28 23:11:12 -0300

    Delete src/components/btn.jsx

--- src/components/btn.jsx
+++
@@ -1,9 +0,0 @@
-import './btn.css'
-
-export const Btn = (
-    {func, text}
-) => {
-    return(
-        <button className="btn" onClick={() => window.location.href=func}>{text}</button>
-    )
-}
