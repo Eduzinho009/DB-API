65a6b42bb0dd15d2e7ca4bdedbe75f58da19f369
Eduzinho009 <115952759+Eduzinho009@users.noreply.github.com>
2025-04-30 14:08:11 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import style from "./App.module.css";
-//import { Btn } from "./components/btn";
-import { Menu } from "./components/menu";
-//import { Card } from "./components/card";
-
-import p1 from "./assets/images/s2-imagen.png";
-
-import logo from "./assets/images/image.png"
-//import { cards } from './assets/mock/cards';
-
 import { useState } from "react";
+import style from "./App.module.css";
+import { Menu } from "./components/menu";
+
+import logo from "./assets/images/image.png";
+
+function VideoToggle() {
+  const [mostrar, setMostrar] = useState(false);
+
+  const alternarVideo = () => {
+    setMostrar(!mostrar);
+  };
+
+  return (
+    <div>
+      <button onClick={alternarVideo} className={style.video}>O que é Dragon Ball?</button>
+      {mostrar && (
+        <div >
+          <iframe
+          className={style.videolegal}
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/VuY0ABWfjSE"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      )}
+    </div>
+  );
+}
 
 function App() {
   const defaultPhoneNumber = "5541997545587";
@@ -25,40 +46,41 @@ function App() {
   };
 
   const handleZap = () => {
-    const { name, email, message } = formData;
+    const { nome, email, Assunto } = formData;
 
     const urlZAPZAP = `https://api.whatsapp.com/send?phone=${defaultPhoneNumber}&text=
-  Nome:%20${name}%0D%0A
+  Nome:%20${nome}%0D%0A
   Email:%20${email}%0D%0A
-  Mensagem:%20${message}%0D%0A`;
+  Assunto:%20${Assunto}%0D%0A`;
 
     window.open(urlZAPZAP, "_blank");
   };
 
   return (
     <>
-      <Menu s1="API" s2="Quer falar com o Dev" style={{width: "100%"}}/>
+      <Menu s1="API" s2="Quer falar com o Dev" style={{ width: "100%" }} />
       <main>
-
-
         <section id="s1" className={style.s1}>
-          <img src={logo} alt="principio" className={style.logoS1}/>
+          <img src={logo} alt="principio" className={style.logoS1} />
           <div className={style.butao}>
-           <button><a href={'/requisicao'} style={{fontSize: "14px"}}>Acessar a nossa API</a></button>
-           </div>
+            <button>
+              <a href={"/requisicao"} style={{ fontSize: "14px" }}>
+                Acessar a nossa API
+              </a>
+            </button>
+          </div>
         </section>
-
 
         <section id="s3" className={style.s2}>
           <h1>Quer falar com o DEV?</h1>
-          <h5>coloque as informações nos blocos de textos e coloque o seu assunto</h5>
-          
+          <h5>Coloque as informações nos blocos de textos e coloque o seu assunto</h5>
+
           <div className={style.inputS2}>
             <input
               type="text"
-              id="name"
-              name="name"
-              value={formData.name}
+              id="nome"
+              name="nome"
+              value={formData.nome}
               onChange={handleChange}
               required
               placeholder="Nome"
@@ -78,11 +100,12 @@ function App() {
           </div>
 
           <div className={style.mensagemS2}>
-            <label htmlFor="message">Assunto:</label><br />
+            <label htmlFor="Assunto">Assunto:</label>
+            <br />
             <textarea
-              name="message"
-              id="message"
-              value={formData.message}
+              name="Assunto"
+              id="Assunto"
+              value={formData.Assunto}
               onChange={handleChange}
               cols="30"
               rows="10"
@@ -90,14 +113,14 @@ function App() {
             ></textarea>
           </div>
 
-        <div className={style.botoesS2}>
-          <button onClick={handleZap} className={style.botaoS2}>Enviar mensagem</button>
-          <a href="https://www.youtube.com/watch?v=VuY0ABWfjSE" style={{fontSize: "14px"}} className={style.queroNota}>o que é dragon ball?</a>
+          <button onClick={handleZap} className={style.botaoS2}>
+              Enviar mensagem
+            </button>
+
+          <div className={style.botoesS2}>
+            <VideoToggle />
           </div>
-
         </section>
-
-
       </main>
     </>
   );
